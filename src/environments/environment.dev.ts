fc852f0294f23ffbb5400cd9db7258c75ec35eb7
Yane Lobo <yanelobo@gmail.com>
2025-12-30 23:47:36 -0300

    Environment config

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   // Replace with the public backend URL after deploy (Render)
   backendUrl: 'https://music-guess-6d95.onrender.com'
 };
